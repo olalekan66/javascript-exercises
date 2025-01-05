29d94de765e0a8568c9f923167e8368a7aef2602
olalekan66 <181458807+olalekan66@users.noreply.github.com>
2025-01-05 16:56:21 +0100

    solved exercise 1 to 5

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+
+    return str.split('').reverse().join('');
 
 };
-
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc! Hello, Odinite.');
+reverseString('');
 // Do not edit below this line
 module.exports = reverseString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+let total = 0;
+while ((Number.isInteger(x) && x>0) && (Number.isInteger(y) && y>0)){
+    if (y>x){
+        for (let i = x; i <= y; i++){
+        total += i;
+        }
+    }
+    else if(x>y){
+        for (let i = y; i <= x; i++){
+        total += i;
+        }
+    }
+return total;
+};
+return 'ERROR';
 };
 
+sumAll(1, 4);
+sumAll(1, 4000);
+sumAll(123, 1);
+sumAll(-10, 4);
+sumAll(2.5, 4);
+sumAll(10, "90");
+sumAll(10, [90, 1]);
 // Do not edit below this line
 module.exports = sumAll;
+
