38843e1d7fe77ca4e584bbf6a5e8bb4c65c68eb4
olalekan66 <181458807+olalekan66@users.noreply.github.com>
2025-01-05 22:16:11 +0100

    completed exercises 1 to 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  const degreeInCelcius = (x - 32) * 5/9;
+  const round = parseFloat(degreeInCelcius.toFixed(1));
+  return round;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+    const degreeInFahrenheit = (tempCel * 9/5) + 32;
+    const rounded = parseFloat(degreeInFahrenheit.toFixed(1));
+    return rounded;
 };
-
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
+convertToFahrenheit(0);
+convertToFahrenheit(73.2);
+convertToFahrenheit(-10);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
